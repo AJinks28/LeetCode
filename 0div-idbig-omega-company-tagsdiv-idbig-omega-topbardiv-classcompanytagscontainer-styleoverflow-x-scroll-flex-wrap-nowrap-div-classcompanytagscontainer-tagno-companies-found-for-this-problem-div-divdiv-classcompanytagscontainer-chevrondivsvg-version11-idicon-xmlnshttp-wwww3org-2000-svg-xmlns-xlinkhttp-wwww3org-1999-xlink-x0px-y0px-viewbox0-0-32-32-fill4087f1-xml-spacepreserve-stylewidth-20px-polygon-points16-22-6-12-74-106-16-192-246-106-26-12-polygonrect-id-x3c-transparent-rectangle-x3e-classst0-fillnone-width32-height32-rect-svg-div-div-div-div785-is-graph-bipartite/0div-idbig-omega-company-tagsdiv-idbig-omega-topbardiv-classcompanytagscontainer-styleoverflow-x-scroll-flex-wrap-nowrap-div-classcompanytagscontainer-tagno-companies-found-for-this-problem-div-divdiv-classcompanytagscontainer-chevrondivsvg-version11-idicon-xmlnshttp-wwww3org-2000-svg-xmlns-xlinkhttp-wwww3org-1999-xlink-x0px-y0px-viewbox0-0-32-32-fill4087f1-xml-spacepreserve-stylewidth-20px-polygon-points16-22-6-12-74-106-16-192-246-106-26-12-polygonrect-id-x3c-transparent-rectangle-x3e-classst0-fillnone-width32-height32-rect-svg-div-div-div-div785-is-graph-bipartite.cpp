2f62af9ecc6bda31913de7f018f0ba5021f923d2
Ajinkya K <133963919+AJinks28@users.noreply.github.com>
2024-03-09 18:01:40 +0530

    Time: 18 ms (58.50%), Space: 16.1 MB (68.81%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div785-is-graph-bipartite/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div785-is-graph-bipartite.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div785-is-graph-bipartite/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div785-is-graph-bipartite.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    bool check(int src,vector<int>& col,int n,vector<vector<int>>& graph){
-        queue<int> q;
-        q.push(src);
-        col[src]=0;//give color 0 to src node
+    //USING DFS
+    bool dfs(int node,int currCol,vector<int>& col,vector<vector<int>>& graph){
+        col[node]=currCol;
         
-        while(!q.empty()){
-            int curr=q.front();
-            q.pop();
-            
-            for(auto it:graph[curr]){
-                //if adjNode is not yet colored, give it a opposite color of current node
-                if(col[it]==-1){
-                    col[it]=!col[curr];
-                    q.push(it);
-                }
-                //if adjNode is already colored and color is same as of curr node--> NOT bipartite
-                else if(col[it]==col[curr])
-                    return false;
+        for(auto adjNode:graph[node]){
+            // if uncoloured
+            if(col[adjNode]==-1){
+                if(dfs(adjNode,!currCol,col,graph)==false) return false;
             }
+            // if previously coloured and have the same colour
+            else if(col[adjNode]==currCol) return false;
         }
         return true;
     }
@@ -30,7 +22,7 @@ public:
         //for all connected components
         for(int i=0;i<n;i++){
             if(col[i]==-1){
-                if(check(i,col,n,graph)==false) return false;
+                if(dfs(i,0,col,graph)==false) return false;
             }
         }
         
