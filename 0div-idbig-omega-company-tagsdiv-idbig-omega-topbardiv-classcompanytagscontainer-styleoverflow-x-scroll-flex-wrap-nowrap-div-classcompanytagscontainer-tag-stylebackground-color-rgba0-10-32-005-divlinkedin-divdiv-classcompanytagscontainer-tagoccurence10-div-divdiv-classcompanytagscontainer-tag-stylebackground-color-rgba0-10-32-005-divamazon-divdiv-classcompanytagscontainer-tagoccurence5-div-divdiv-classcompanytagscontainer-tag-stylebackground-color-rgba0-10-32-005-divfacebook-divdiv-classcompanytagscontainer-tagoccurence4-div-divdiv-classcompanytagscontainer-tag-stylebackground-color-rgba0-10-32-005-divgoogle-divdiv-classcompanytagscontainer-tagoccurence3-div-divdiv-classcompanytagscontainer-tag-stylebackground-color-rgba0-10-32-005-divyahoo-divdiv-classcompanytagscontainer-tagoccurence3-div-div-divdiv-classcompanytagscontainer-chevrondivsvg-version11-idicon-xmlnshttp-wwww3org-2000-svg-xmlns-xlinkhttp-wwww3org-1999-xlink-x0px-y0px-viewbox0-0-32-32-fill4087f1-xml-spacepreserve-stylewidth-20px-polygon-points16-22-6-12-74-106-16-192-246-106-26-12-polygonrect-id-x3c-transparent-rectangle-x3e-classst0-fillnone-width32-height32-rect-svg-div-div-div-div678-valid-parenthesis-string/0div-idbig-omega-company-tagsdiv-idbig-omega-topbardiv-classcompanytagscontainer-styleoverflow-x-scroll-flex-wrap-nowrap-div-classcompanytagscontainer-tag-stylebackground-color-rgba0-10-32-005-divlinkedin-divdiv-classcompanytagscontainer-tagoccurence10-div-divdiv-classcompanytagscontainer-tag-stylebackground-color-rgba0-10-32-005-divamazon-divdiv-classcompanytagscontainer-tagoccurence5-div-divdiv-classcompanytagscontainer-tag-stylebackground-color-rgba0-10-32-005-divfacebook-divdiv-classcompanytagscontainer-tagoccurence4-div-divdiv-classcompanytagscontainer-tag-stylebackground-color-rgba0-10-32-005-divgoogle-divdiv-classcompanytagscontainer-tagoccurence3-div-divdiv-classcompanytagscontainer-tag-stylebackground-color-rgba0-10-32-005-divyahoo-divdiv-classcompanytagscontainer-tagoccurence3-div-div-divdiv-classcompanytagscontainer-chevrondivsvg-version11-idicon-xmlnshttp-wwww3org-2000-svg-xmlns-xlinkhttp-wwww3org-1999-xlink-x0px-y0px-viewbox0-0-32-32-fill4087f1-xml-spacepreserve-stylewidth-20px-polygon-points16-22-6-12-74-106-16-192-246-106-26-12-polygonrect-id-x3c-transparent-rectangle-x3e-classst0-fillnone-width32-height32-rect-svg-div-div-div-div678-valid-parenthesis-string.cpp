3c0dff4fe4ee6cdba91f29a35333e9ab646e3cb8
Ajinkya K <133963919+AJinks28@users.noreply.github.com>
2024-04-07 12:17:59 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (75.32%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divyahoo-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div678-valid-parenthesis-string/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divyahoo-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div678-valid-parenthesis-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int cmax=0,cmin=0;
+        
+        for(auto c: s){
+            if(c=='('){
+                cmax++;
+                cmin++;
+            }
+            if(c==')'){
+                cmax--;
+                cmin=max(0,cmin-1);
+            }
+            if(c=='*'){
+                cmax++;
+                cmin=max(0,cmin-1);
+            }
+            if(cmax<0) return false;
+        }
+        return cmin==0;
+    }
+};
+// cmax considers each '*' as '(', which should never be negative
+// cmin considers each '*' as ')' as much as possible (treat it as empty string if cmin<0 -- "max(cmin-1, 0)").
+// In the end, cmin should be 0. If it's larger than 0, it means even if we consider every '*' as ')', there is still some '(' left.
+
+// cmax counts the maximum open parenthesis,
+// which means the maximum number of unbalanced '(' that COULD be paired.
+// cmin counts the minimum open parenthesis,
+// which means the number of unbalanced '(' that MUST be paired.
