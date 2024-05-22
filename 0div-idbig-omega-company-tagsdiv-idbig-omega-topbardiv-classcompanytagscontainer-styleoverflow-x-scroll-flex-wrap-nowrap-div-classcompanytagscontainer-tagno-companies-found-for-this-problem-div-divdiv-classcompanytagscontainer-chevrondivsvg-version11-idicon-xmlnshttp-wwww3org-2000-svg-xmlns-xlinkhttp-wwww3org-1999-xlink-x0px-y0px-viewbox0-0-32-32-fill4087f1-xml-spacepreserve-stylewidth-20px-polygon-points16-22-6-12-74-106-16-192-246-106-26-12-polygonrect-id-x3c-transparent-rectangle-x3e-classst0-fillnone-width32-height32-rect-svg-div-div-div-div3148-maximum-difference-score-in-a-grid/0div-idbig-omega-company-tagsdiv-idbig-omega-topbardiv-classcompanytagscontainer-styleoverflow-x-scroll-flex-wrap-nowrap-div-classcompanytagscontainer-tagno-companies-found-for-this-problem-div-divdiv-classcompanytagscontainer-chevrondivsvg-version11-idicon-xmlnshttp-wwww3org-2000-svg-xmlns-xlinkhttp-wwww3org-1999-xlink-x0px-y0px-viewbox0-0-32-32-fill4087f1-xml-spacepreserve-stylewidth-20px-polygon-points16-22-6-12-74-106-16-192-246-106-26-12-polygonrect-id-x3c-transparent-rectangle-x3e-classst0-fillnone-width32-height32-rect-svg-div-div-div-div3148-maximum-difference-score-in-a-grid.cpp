3c34ac030b36d9c951cbfd4ad0ca52920910e53b
Ajinkya K <133963919+AJinks28@users.noreply.github.com>
2024-05-22 09:25:40 +0530

    Time: 142 ms (67.31%), Space: 56.6 MB (96.48%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3148-maximum-difference-score-in-a-grid/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3148-maximum-difference-score-in-a-grid.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3148-maximum-difference-score-in-a-grid/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3148-maximum-difference-score-in-a-grid.cpp
@@ -5,16 +5,21 @@ public:
         int n=grid[0].size();
         
         int ans=INT_MIN;
-        vector<vector<int>> dp(m,vector<int>(n));
+        // vector<vector<int>> dp(m,vector<int>(n));
+        
+        //dp will store min elem topmost left area
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                dp[i][j]=INT_MAX;
-                if(i>0) dp[i][j]=min(dp[i][j],dp[i-1][j]);
-                if(j>0) dp[i][j]=min(dp[i][j],dp[i][j-1]);
+                // dp[i][j]=INT_MAX;
+                int mn=INT_MAX;
+                if(i>0) mn=min(mn,grid[i-1][j]);//dp[i][j]=min(dp[i][j],dp[i-1][j]);
+                if(j>0) mn=min(mn,grid[i][j-1]);//dp[i][j]=min(dp[i][j],dp[i][j-1]);
                 
-                ans=max(ans,grid[i][j]-dp[i][j]);
-                dp[i][j]=min(dp[i][j],grid[i][j]);
+                // ans=max(ans,grid[i][j]-dp[i][j]);
+                //dp[i][j]=min(dp[i][j],grid[i][j]);
+                ans=max(ans,grid[i][j]-mn);
+                grid[i][j]=min(grid[i][j],mn);
             }
         }
         return ans;
