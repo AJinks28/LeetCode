351a252e1518b5de58552d3c9db47dafdd33f18b
Ajinkya K <133963919+AJinks28@users.noreply.github.com>
2024-06-08 09:55:21 +0530

    Time: 142 ms (64.84%), Space: 167 MB (46.97%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2771-longest-non-decreasing-subarray-from-two-arrays/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2771-longest-non-decreasing-subarray-from-two-arrays.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2771-longest-non-decreasing-subarray-from-two-arrays/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2771-longest-non-decreasing-subarray-from-two-arrays.cpp
@@ -1,52 +1,17 @@
 class Solution {
 public:
-    
-//     int solve(int idx,int prevArr,vector<int>& nums1, vector<int>& nums2,vector<vector<int>> &dp){
-//         if(idx>=nums1.size()) return 0;
-//         if(dp[idx][prevArr]!=-1) return dp[idx][prevArr];
-//         int ans=0;
-//         if(prevArr==0){
-//             ans=max({solve(idx+1,0,nums1,nums2,dp),1+solve(idx+1,1,nums1,nums2,dp),
-//                      1+solve(idx+1,2,nums1,nums2,dp)});
-//         }
-//         else if(prevArr==1){
-//             if(nums1[idx-1]<=nums1[idx]) ans=max(ans,1+solve(idx+1,1,nums1,nums2,dp));
-//             if(nums1[idx-1]<=nums2[idx]) ans=max(ans,1+solve(idx+1,2,nums1,nums2,dp));
-//         }
-//         else{
-//             if(nums2[idx-1]<=nums1[idx]) ans=max(ans,1+solve(idx+1,1,nums1,nums2,dp));
-//             if(nums2[idx-1]<=nums2[idx]) ans=max(ans,1+solve(idx+1,2,nums1,nums2,dp));
-//         }
-        
-//         return dp[idx][prevArr]=ans;
-//     }
-    
     int maxNonDecreasingLength(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size();
-        vector<vector<int>> dp(n+1,vector<int>(3,0));
         
-        // return solve(0,0,nums1,nums2,dp);
-        
-        dp[n][0]=dp[n][1]=dp[n][2]=0;
-        for(int idx=n-1;idx>=0;idx--){
-            
-            for(int j=0;j<3;j++){
-                int ans=0;
-                if(j==0) ans=max({dp[idx+1][0],1+dp[idx+1][1],1+dp[idx+1][2]});
-                else if(j==1){
-                     if(idx>0 && nums1[idx-1]<=nums1[idx]) ans=max(ans,1+dp[idx+1][1]);
-                     if(idx>0 && nums1[idx-1]<=nums2[idx]) ans=max(ans,1+dp[idx+1][2]);
-                }
-                else{
-                     if(idx>0 && nums2[idx-1]<=nums1[idx]) ans=max(ans,1+dp[idx+1][1]);
-                     if(idx>0 && nums2[idx-1]<=nums2[idx]) ans=max(ans,1+dp[idx+1][2]);
-                }
-                dp[idx][j]=ans;
-            }
-            
+        vector<int> dp1(n+1,1),dp2(n+1,1);
+        int maxLen=1;
+        for(int i=1;i<n;i++){
+            if(nums1[i]>=nums1[i-1]) dp1[i]=max(dp1[i],1+dp1[i-1]);
+            if(nums1[i]>=nums2[i-1]) dp1[i]=max(dp1[i],1+dp2[i-1]);
+            if(nums2[i]>=nums1[i-1]) dp2[i]=max(dp2[i],1+dp1[i-1]);
+            if(nums2[i]>=nums2[i-1]) dp2[i]=max(dp2[i],1+dp2[i-1]);
+            maxLen=max({maxLen,dp1[i],dp2[i]});
         }
-        
-        return dp[0][0];
-        
+        return maxLen;
     }
 };
