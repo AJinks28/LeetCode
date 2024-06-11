09c77b06952c3512c7c870e8ef27a0331e29a10a
Ajinkya K <133963919+AJinks28@users.noreply.github.com>
2024-06-11 19:57:04 +0530

    Time: 10 ms (76.95%), Space: 13.6 MB (51.27%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div931-minimum-falling-path-sum/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div931-minimum-falling-path-sum.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div931-minimum-falling-path-sum/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div931-minimum-falling-path-sum.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>& dp){
-        int m=matrix.size();
-        int n=matrix[0].size();
+    int minFallingPathSum(vector<vector<int>>& mat) {
+        int n=mat.size();
         
-        if(j<0 || j>=m) return INT_MAX;
-        if(i==0) return dp[i][j]=matrix[0][j];
+        vector<vector<int>> dp(n+1,vector<int>(n+1,INT_MAX));
         
-        if(dp[i][j]!=-1e9) return dp[i][j];
+        for(int i=0;i<n;i++) dp[0][i]=mat[0][i];
         
-        int up=solve(i-1,j,matrix,dp);
-        int leftD=solve(i-1,j-1,matrix,dp);
-        int rightD=solve(i-1,j+1,matrix,dp);
-                
-        return dp[i][j]=matrix[i][j]+min(up,min(leftD,rightD));
-             
-    }
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        //Variable Start and Variable ending problem
-        int m=matrix.size();
-        int n=matrix[0].size();
-        
-        vector<vector<int>> dp(m,vector<int>(n,-1e9));
-        
-        for(int i=0;i<m;i++){
+        for(int i=1;i<n;i++){
             for(int j=0;j<n;j++){
-                solve(i,j,matrix,dp);
+                dp[i][j]=dp[i-1][j];
+                if(j>0) dp[i][j]=min(dp[i][j],dp[i-1][j-1]);
+                if(j<n-1) dp[i][j]=min(dp[i][j],dp[i-1][j+1]);
+                
+                dp[i][j]+=mat[i][j];
             }
         }
-        int minAns=dp[m-1][0];
-        for(int j=0;j<m;j++) minAns=min(minAns,dp[m-1][j]);
+        int res=INT_MAX;
+        for(int i=0;i<n;i++) res=min(res,dp[n-1][i]);
         
-        return minAns;
+        return res;
     }
 };
