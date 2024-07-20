3c10ce938822810752448b007aacee7c3f0656c8
Ajinkya K <133963919+AJinks28@users.noreply.github.com>
2024-07-20 18:15:55 +0530

    Time: 41 ms (34.09%), Space: 36.1 MB (11.51%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
@@ -7,19 +7,9 @@ public:
         vector<vector<int>> mat(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(rowSum[i]>colSum[j]){
-                    mat[i][j]=colSum[j];
-                    rowSum[i]-=colSum[j];
-                    colSum[j]=0;
-                }else if(rowSum[i]<colSum[j]){
-                    mat[i][j]=rowSum[i];
-                    colSum[j]-=rowSum[i];
-                    rowSum[i]=0;
-                }else{
-                    mat[i][j]=rowSum[i];
-                    rowSum[i]=0;
-                    colSum[j]=0;
-                }
+                mat[i][j]=min(rowSum[i],colSum[j]);
+                rowSum[i] -= mat[i][j];
+                colSum[j] -= mat[i][j];
             }
         }
         return mat;
