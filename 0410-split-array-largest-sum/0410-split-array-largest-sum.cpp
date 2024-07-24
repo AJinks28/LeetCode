46e6c22aba04a596033a75003fa067f17b12813b
Ajinkya K <133963919+AJinks28@users.noreply.github.com>
2024-07-24 15:15:45 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (24.64%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    bool canSplit(int d,vector<int>& nums,int k){
+    int findMinSum(int k,vector<int>& nums){
         int cnt=0;
-        int totSum=0;
+        int sum=0;
         for(int i=0;i<nums.size();i++){
-            if(totSum+nums[i]<=d) totSum+=nums[i];
-            else {
+            if(sum+nums[i]>k){
                 cnt++;
-                totSum=nums[i];
+                sum=nums[i];
             }
-            
+            else{sum+=nums[i];}
         }
-        if(cnt+1<=k) return true;
-        return false;
+        return cnt+1;
     }
     int splitArray(vector<int>& nums, int k) {
-        int n=nums.size();
         
-        int l=0;
-        int r=0;
-        
-        for (int num : nums) {
-            l = max(l,num);
-            r += num;
+        int l=0,r=0;
+        for(int &n:nums) {
+            r+=n;
+            l=max(l,n);
         }
-        
-        while(l<r){
-            int mid=l+(r-l)/2;
-            
-            if(canSplit(mid,nums,k)){
-                r=mid;
+        int res=0;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(findMinSum(mid,nums)>k){
                 
+                l=mid+1;
             }
-            else l=mid+1;
+            else{ res=mid;r=mid-1; }
         }
-        return l;
+        return res;
     }
 };
