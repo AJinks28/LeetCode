e533d30aa32825b132a2d33c83d5fce59979b356
Ajinkya K <133963919+AJinks28@users.noreply.github.com>
2024-07-24 11:54:51 +0530

    Time: 47 ms (15.04%), Space: 33.9 MB (16.23%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1011-capacity-to-ship-packages-within-d-days/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findDays(int cap,vector<int>& w){
+        int d=1;
+        int sum=0;
+        for(int i=0;i<w.size();i++){
+            if(w[i]>cap) return 1e9;
+            if(sum+w[i]>cap){
+                d++;
+                sum=w[i];
+            }
+            else sum+=w[i];
+        }
+        return d;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+        int l=1,r=0;
+        
+        for(int &w:weights) r+=w;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            
+            if(findDays(mid,weights)<=days) r=mid;
+            else l=mid+1;
+        }
+        return l;
+    }
+};
