16c7847c7878a933fba51ae500fd1be39b6d4787
Ajinkya K <133963919+AJinks28@users.noreply.github.com>
2024-08-03 16:42:07 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (18.62%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    int findMinSum(int k,vector<int>& nums){
+    int solve(int mid,vector<int>& nums){
         int cnt=0;
         int sum=0;
         for(int i=0;i<nums.size();i++){
-            if(sum+nums[i]>k){
+            if(sum+nums[i]>mid){
                 cnt++;
                 sum=nums[i];
             }
-            else{sum+=nums[i];}
+            else sum+=nums[i];
         }
         return cnt+1;
     }
     int splitArray(vector<int>& nums, int k) {
+        int n=nums.size();
         
         int l=0,r=0;
-        for(int &n:nums) {
-            r+=n;
-            l=max(l,n);
+        for(int num:nums) {
+            l=max(l,num);
+            r+=num;
         }
-        int res=0;
+        
         while(l<=r){
             int mid=(l+r)/2;
-            if(findMinSum(mid,nums)>k){
-                
-                l=mid+1;
-            }
-            else{ res=mid;r=mid-1; }
+            if(solve(mid,nums)<=k) r=mid-1;
+            else l=mid+1;
         }
-        return res;
+        return l;
     }
 };
