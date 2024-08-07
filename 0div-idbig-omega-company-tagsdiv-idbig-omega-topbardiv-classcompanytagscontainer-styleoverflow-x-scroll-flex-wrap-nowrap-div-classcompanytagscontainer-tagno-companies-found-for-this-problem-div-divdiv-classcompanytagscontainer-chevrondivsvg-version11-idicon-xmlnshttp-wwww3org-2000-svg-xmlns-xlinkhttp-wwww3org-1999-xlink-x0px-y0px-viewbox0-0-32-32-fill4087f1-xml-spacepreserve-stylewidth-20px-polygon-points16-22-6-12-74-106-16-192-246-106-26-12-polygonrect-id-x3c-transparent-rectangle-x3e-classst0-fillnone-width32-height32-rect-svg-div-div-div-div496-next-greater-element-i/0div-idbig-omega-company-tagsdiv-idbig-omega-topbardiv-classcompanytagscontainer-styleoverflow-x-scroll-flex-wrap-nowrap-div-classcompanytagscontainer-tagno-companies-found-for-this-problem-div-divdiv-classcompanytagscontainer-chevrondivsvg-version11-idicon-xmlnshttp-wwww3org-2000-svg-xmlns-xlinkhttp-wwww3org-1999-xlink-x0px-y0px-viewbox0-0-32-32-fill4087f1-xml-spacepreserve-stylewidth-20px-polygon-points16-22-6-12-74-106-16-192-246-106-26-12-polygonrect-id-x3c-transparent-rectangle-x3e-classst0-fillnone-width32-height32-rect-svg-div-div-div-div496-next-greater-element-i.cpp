24c2c69189d3e768b30911590329378d65f340dd
Ajinkya K <133963919+AJinks28@users.noreply.github.com>
2024-08-07 15:36:38 +0530

    Time: 8 ms (31.16%), Space: 12 MB (62.41%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div496-next-greater-element-i/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div496-next-greater-element-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size(),n2=nums2.size();
+        
+        stack<int> st;
+        map<int,int> nge;
+        for(int i=n2-1;i>=0;i--){
+            while(!st.empty() && st.top()<nums2[i]) st.pop();
+            if(st.empty()) nge[nums2[i]]=-1;
+            else nge[nums2[i]]=st.top();
+            st.push(nums2[i]);
+        }
+        vector<int> ans;
+        for(int i=0;i<n1;i++) ans.push_back(nge[nums1[i]]);
+        
+        return ans;
+    }
+};
