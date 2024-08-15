7bc1c2709f3935cd54e80bf7bcd55ffe7273f24d
Ajinkya K <133963919+AJinks28@users.noreply.github.com>
2024-08-15 23:48:21 +0530

    Time: 2 ms (70.44%), Space: 8.2 MB (54.83%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeOuterParentheses(string S) {
+        string res;
+        int opened = 0;
+        for (char c : S) {
+            if (c == '(' ) {
+                if(opened > 0) res += c;
+                opened++;
+            }
+            if (c == ')') {
+                if(opened > 1) res += c;
+                opened--;
+            }
+        }
+        return res;
+    }
+};
