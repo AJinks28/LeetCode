3d2bdd4ccdbd1f66ec930f7ba6119989f78830a1
Ajinkya K <133963919+AJinks28@users.noreply.github.com>
2024-08-17 17:11:00 +0530

    Time: 120 ms (43.38%), Space: 26.6 MB (40.01%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if (s.length() <= 1) {
+        int n=s.size();
+        if (n <= 1) {
             return s;
         }
         
         int max_len = 1;
         int start = 0;
         int end = 0;
-        vector<std::vector<bool>> dp(s.length(), std::vector<bool>(s.length(), false));
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
         
-        for (int i = 0; i < s.length(); ++i) {
+        for (int i = 0; i < n; ++i) {
             dp[i][i] = true;
             for (int j = 0; j < i; ++j) {
+                //expanding around center
                 if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    //i - j <= 2 means either 1 or 2 bcz center can be of len 1 or 2
                     dp[j][i] = true;
                     if (i - j + 1 > max_len) {
                         max_len = i - j + 1;
