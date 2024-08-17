638f78b095fe1f6bda6583ec7a1adade43be9084
Ajinkya K <133963919+AJinks28@users.noreply.github.com>
2024-08-17 17:50:52 +0530

    Time: 9 ms (16.00%), Space: 23.6 MB (9.37%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n=s.size();
+        stringstream ss(s);
+        string token,res;
         
-        int i=0;//to iterate s
-        int l=0;//l is start of curr word
-        int r=0;//r is end of curr word
-        reverse(s.begin(), s.end());
-        while(i<n){
-            
-            while(i<n && s[i]!=' '){
-                s[r]=s[i];
-                r++;
-                i++;
-            }
-            if (l < r) { // if we can find a non-empty word then
-                reverse(s.begin() + l, s.begin() + r); // reverse current word
-                if (r == n) break;
-                s[r] = ' '; // set empty space
-                r++;
-                l = r;
-            }
-            ++i; // now i == n or s[i] == ' ', so we skip that character!
+        while(ss>>token){
+            res= token+" "+res;
         }
-         if (r > 0 && s[r-1] == ' ') --r; // skip last empty character if have
-        s.resize(r);
-        return s;
+        return res.substr(0,res.size()-1);
     }
 };
