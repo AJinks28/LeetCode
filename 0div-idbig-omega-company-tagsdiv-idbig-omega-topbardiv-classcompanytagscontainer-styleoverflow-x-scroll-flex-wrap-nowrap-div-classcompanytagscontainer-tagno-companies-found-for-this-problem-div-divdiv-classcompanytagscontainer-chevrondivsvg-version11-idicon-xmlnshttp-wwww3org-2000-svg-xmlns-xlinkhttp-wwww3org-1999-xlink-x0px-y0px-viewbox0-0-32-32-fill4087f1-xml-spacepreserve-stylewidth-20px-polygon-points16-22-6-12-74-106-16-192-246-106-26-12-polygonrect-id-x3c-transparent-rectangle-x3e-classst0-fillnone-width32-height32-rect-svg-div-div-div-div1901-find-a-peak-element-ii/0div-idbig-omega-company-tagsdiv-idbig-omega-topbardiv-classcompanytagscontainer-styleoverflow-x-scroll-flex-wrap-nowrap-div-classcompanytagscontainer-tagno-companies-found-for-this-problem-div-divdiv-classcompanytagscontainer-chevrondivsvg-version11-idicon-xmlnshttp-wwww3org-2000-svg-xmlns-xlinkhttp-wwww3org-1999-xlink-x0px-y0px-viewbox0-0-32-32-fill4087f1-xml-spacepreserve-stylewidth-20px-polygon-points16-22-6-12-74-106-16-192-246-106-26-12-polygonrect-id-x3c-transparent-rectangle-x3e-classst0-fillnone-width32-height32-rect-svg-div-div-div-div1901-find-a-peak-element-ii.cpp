277d3947f0019acd5091f025e7bcd12f30cc8403
Ajinkya K <133963919+AJinks28@users.noreply.github.com>
2024-08-20 22:40:53 +0530

    Time: 130 ms (22.54%), Space: 49.3 MB (27.55%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1901-find-a-peak-element-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1901-find-a-peak-element-ii.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1901-find-a-peak-element-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1901-find-a-peak-element-ii.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
+    int findMax(int mid,vector<vector<int>>& mat){
+        int m=mat.size();
+        int mx=mat[0][mid],idx=0;
+        for(int i=0;i<m;i++) { 
+            if(mx<mat[i][mid]){
+                mx=mat[i][mid];
+                idx=i;
+            } 
+        }
+        return idx;
+    }
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int m=mat.size();
         int n=mat[0].size();
         
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if((i-1>=0 && mat[i-1][j]>=mat[i][j]) 
-                  || (j-1>=0 && mat[i][j-1]>=mat[i][j])
-                  || (i+1<m && mat[i+1][j]>=mat[i][j])
-                  || (j+1<n && mat[i][j+1]>=mat[i][j]))
-                    continue;
-                return {i,j};
-            }
+        int l=0,r=n-1;
+        
+        while(l<=r){
+            int mid=(l+r)/2;
+            
+            int rmax=findMax(mid,mat);
+            
+            int left=  mid-1>=0 ? mat[rmax][mid-1] : -1;
+            int right= mid+1 <n ? mat[rmax][mid+1] : -1;
+            
+            if(mat[rmax][mid] > left && mat[rmax][mid] > right)
+                return {rmax,mid};
+            else if(mat[rmax][mid] < left) r=mid-1;
+            else l=mid+1;
         }
         return {-1,-1};
     }
